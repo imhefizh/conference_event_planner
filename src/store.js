97cf202bee852c698f87def1fc37269438fa8f6b
Maulana Hafidz Ismail <hafidzsml@gmail.com>
2025-02-01 17:01:14 +0700

    complete

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // store.js
-import { configureStore } from '@reduxjs/toolkit';
-import venueReducer from './venueSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import venueReducer from "./venueSlice";
+import adsReducer from "./avSlice";
+import mealReducer from "./mealsSlice";
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: adsReducer,
+    meals: mealReducer,
   },
 });
